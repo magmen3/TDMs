4f552780e95baf59940f96256e79200e3261e8d7
Mihail <akovlevmihail535@gmail.com>
2024-01-31 20:22:26 +0400

    Update babashka.lua

--- babashka.lua
+++ babashka.lua
@@ -1,1 +1,24 @@
-print(1)
+-- Программа для проверки, является ли число простым
+
+-- Функция для определения простого числа
+function isPrime(number)
+    if number <= 1 then
+        return false
+    end
+    for i = 2, number - 1 do
+        if number % i == 0 then
+            return false
+        end
+    end
+    return true
+end
+
+-- Число для проверки
+local num = 17
+
+-- Вызов функции и вывод результата
+if isPrime(num) then
+    print(num .. " является простым числом")
+else
+    print(num .. " не является простым числом")
+end
